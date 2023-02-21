0e88e6cef273940171eac984b60fa733efdab08f
Jerlyn Camille Pascua <jppascua@student.apc.edu.ph>
2023-02-21 09:53:54 +0800

    Uploading Personal Profile

---
+++ ci4/app/Views/pages/mysql_connect.php
@@ -0,0 +1,51 @@
+
+<?php
+$name = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email');
+$message = filter_input(INPUT_POST, 'message');
+
+if (!empty($name)){
+  if (!empty($email)){
+    if (!empty($message)){
+
+
+      $host = "localhost";
+      $dbusername = "webprogmi211";
+      $dbpassword = "webprogmi211";
+      $dbname = "webprogmi211";
+      // Create connection
+      $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO jppascua_myGuests (name, email, messages)
+values ('$name','$email','$messages')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+      else{
+        echo "Message should not be empty";
+        die();
+      }
+}
+    else{
+    echo "Email should not be empty";
+    die();
+    }
+}
+else{
+echo "Name should not be empty";
+die();
+}
+
+?>
